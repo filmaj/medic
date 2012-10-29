22c869092d66e09b3bbbb7fcb71cba139a9a09e2
Fil Maj <maj.fil@gmail.com>
2012-10-28 21:41:05 -0700

    added fruitstrap as dependency

--- bootstrap.js
+++ bootstrap.js
@@ -2,7 +2,8 @@ var shell = require('shelljs'),
     fs    = require('fs'),
     path  = require('path');
 
-var libs = ['incubator-cordova-android', 'incubator-cordova-mobile-spec'];
+// Get the cordova libraries
+var libs = ['incubator-cordova-android', 'incubator-cordova-mobile-spec', 'incubator-cordova-ios'];
 
 var libDir = path.join(__dirname, 'lib');
 shell.mkdir('-p', libDir);
@@ -17,3 +18,16 @@ libs.forEach(function(lib) {
         shell.exec(cmd, {silent:true});
     }
 });
+
+// Build fruitstrap
+var cmd = 'cd ./node_modules/fruitstrap && make fruitstrap';
+var make = shell.exec(cmd, {silent:true});
+if (make.code > 0) {
+    console.log('There was an error building fruitstrap. You will not be able to deploy to iOS devices. Suggest you run:');
+    console.log('    cd node_modules/fruitstrap');
+    console.log('    make fruitstrap');
+    console.log('..and fix that if you want to get this to run.');
+} else {
+    console.log('fruitstrap built successfully.');
+}
+
