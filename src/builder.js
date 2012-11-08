c952b37b30b067fe84ba686b11267e9ffb283fa7
Fil Maj <maj.fil@gmail.com>
2012-11-07 19:37:42 -0800

    builder only builds what specified in commits

--- src/builder.js
+++ src/builder.js
@@ -1,13 +1,26 @@
-var android_build          = require('./src/makers/android'),
-    ios_build              = require('./src/makers/ios');
+var android_build = require('./src/makers/android'),
+    ios_build     = require('./src/makers/ios');
 
-// where we store generated apps
-var android_path = path.join(__dirname, '..', 'temp', 'android');
-var ios_path = path.join(__dirname, '..', 'temp', 'ios');
+// where we store generated apps mapping
+var paths = {
+    'incubator-cordova-android':path.join(__dirname, '..', 'temp', 'android'),
+    'incubator-cordova-ios':path.join(__dirname, '..', 'temp', 'ios')
+};
+
+// builder mapping
+var builders = {
+    'incubator-cordova-android':android_build,
+    'incubator-cordova-ios':ios_build
+};
 
 module.export = function builder(commits) {
-    // TODO: only build whats specified in commits
+    // commits format:
+    // { incubator-cordova-android:'sha',
+    //   incubator-cordova-ios:'sha' }
     // TODO: other platforms
-    android_build(android_path);
-    ios_build(ios_path);
+    for (var lib in commits) if (commits.hasOwnProperty(lib)) {
+        if (builders.hasOwnProperty(lib)) {
+            builders[lib](paths[lib]);
+        }
+    }
 };
