d10f74de9bad6604b6951d82f48d8b1c8012c312
Fil Maj <maj.fil@gmail.com>
2012-11-11 18:36:56 -0800

    blackberry scanner module. factored out error writing.

---
+++ src/makers/blackberry/blackberry_scanner.js
@@ -0,0 +1,38 @@
+var path = require('path'),
+    shell = require('shelljs');
+
+module.exports = function blackberry_scanner(config, callback) {
+    var range = config.networkRange;
+    var sdk_path = config.sdk;
+    var deploy = path.join(sdk_path, 'dependencies', 'tools', 'bin', 'blackberry-deploy');
+
+    // figure out over what range of ips to scan
+    var ips = range.split('-');
+    var low = parseInt(ips[0].split('.')[3]);
+    var high = parseInt(ips[1].split('.')[3]);
+    var base = ips[0].split('.').slice(0,3).join('.');
+    var pings = high - low;
+    var devices = null;
+    for (var i = low; i <= high; i++) {
+        (function(last) {
+            var ip = base + '.' + last;
+            var cmd = deploy + ' -listDeviceInfo ' + ip + ' -password password';
+            shell.exec(cmd,{silent:true,async:true},function(code, output) {
+                pings--;
+                if (code === 0) {
+                    var lines = output.split('\n');
+                    var version = lines.filter(function(l) { return l.indexOf('scmbundle::') > -1; })[0].split('::')[1];
+                    var hardware = lines.filter(function(l) { return l.indexOf('hardwareid::') > -1; })[0].split('::')[1];
+                    if (!devices) devices = {};
+                    devices[ip] = {
+                        hardware:hardware,
+                        version:version
+                    };
+                } 
+                if (pings === 0) {
+                    callback(devices);
+                }
+            });
+        }(i));
+    }
+};
---
+++ src/makers/error_writer.js
@@ -0,0 +1,17 @@
+var path = require('path'),
+    fs   = require('fs');
+
+var posts = path.join(__dirname, '..', '..', 'posts');
+
+module.exports = function error_writer(platform, sha, failure, details) {
+    var filename = path.join(posts, platform, sha, new Date().valueOf() + '.json');
+    var contents = JSON.stringify({
+        failure:failure,
+        details:details
+    });
+    fs.writeFile(filename, contents, 'utf-8', function(err) {
+        if (err) throw ('Failed to write out error file to ' + filename);
+        console.error('[ERROR] [' + platform[0].toUpperCase() + platform.substr(1) + '] (sha: ' + sha.substr(0,7) +')');
+        console.error(failure);
+    });
+}
