20b66c4a3d386031a5e1849deb24676839b2a34f
Fil Maj <maj.fil@gmail.com>
2012-11-22 10:39:04 -0800

    fixing bootstrap

--- bootstrap.js
+++ bootstrap.js
@@ -8,7 +8,7 @@ shell.mkdir('-p', libDir);
 
 var contents = fs.readdirSync(libDir);
 
-for (var lib in libs) {
+for (var lib in libs.paths) {
     if (contents.indexOf(lib) == -1) {
         // Don't have the lib, get it.
         var cmd = 'git clone https://git-wip-us.apache.org/repos/asf/' + lib + '.git ' + path.join(libDir, lib);
