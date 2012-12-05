f90400bf2a792f9fe8f358016f7a8f4ef1839e4a
Fil Maj <maj.fil@gmail.com>
2012-12-04 16:33:23 -0800

    adding google analytics

--- src/dashboard/templates/render.js
+++ src/dashboard/templates/render.js
@@ -10,6 +10,7 @@ template    += '<h2>cordova-ios</h2>';
 template    += '{ios}';
 template    += '<h2>cordova-blackberry</h2>';
 template    += '{blackberry}';
+template    += '<script type="text/javascript">var _gaq = _gaq || [];_gaq.push([\'_setAccount\', \'UA-36811541-1\']);_gaq.push([\'_trackPageview\']);(function() {var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);})();</script>';
 template    += '</body></html>';
 
 // template for the commit table
