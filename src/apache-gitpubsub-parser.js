f815d010830b572d6941f94a4a12ff6d18245639
Fil Maj <maj.fil@gmail.com>
2013-01-11 18:37:01 -0800

    try/catch around the gitpubsub parser in case something happens.

--- src/apache-gitpubsub-parser.js
+++ src/apache-gitpubsub-parser.js
@@ -6,18 +6,23 @@ var Stream    = require('stream').Stream,
 var s = new Stream();
 s.writable = true;
 s.write = function(data) {
-    var json = data.toString();
-    json = json.substr(0,json.length-3); // get rid of trailing comma
-    json = JSON.parse(json);
-    if (json.stillalive) return true; // if its just a keepalive ping ignore it
-    if (json.commit) {
-        if (json.commit.project in libraries.paths) {
-            var job = {};
-            job[json.commit.project] = {
-                sha:json.commit.sha
-            };
-            queue.push(job);
+    try {
+        var json = data.toString();
+        json = json.substr(0,json.length-3); // get rid of trailing comma
+        json = JSON.parse(json);
+        if (json.stillalive) return true; // if its just a keepalive ping ignore it
+        if (json.commit) {
+            if (json.commit.project in libraries.paths) {
+                var job = {};
+                job[json.commit.project] = {
+                    sha:json.commit.sha
+                };
+                queue.push(job);
+            }
         }
+    } catch(e) {
+        console.error('[GITPUBSUB] [ERROR]: ' + e.message);
+        console.log('[GITPUBSUB] Continuing.');
     }
     return true;
 };
